b24a6888495834d76635a3346b7d4d01fb636ac9
LM <git@marketos.com>
2021-11-08 11:45:37 -0700

    Updated dot files with changes from work

---
+++ bash/dot_mybash/keybinds/history.bash
@@ -0,0 +1,6 @@
+#/bin/bash
+if [[ $- == *i* ]]
+then
+  bind '"\e[A": history-search-backward'
+  bind '"\e[B": history-search-forward'
+fi
--- bash/dot_mybash/vars/prompt.bash
+++ bash/dot_mybash/vars/prompt.bash
@@ -31,19 +31,19 @@ function git_color {
   fi
 }
 
-function git_branch {
-  local git_status="$(git status 2> /dev/null)"
-  local on_branch="On branch ([^${IFS}]*)"
-  local on_commit="HEAD detached at ([^${IFS}]*)"
-
-  if [[ $git_status =~ $on_branch ]]; then
-    local branch=${BASH_REMATCH[1]}
-    echo "($branch)"
-  elif [[ $git_status =~ $on_commit ]]; then
-    local commit=${BASH_REMATCH[1]}
-    echo "($commit)"
-  fi
-}
+#function git_branch {
+#  local git_status="$(git status 2> /dev/null)"
+#  local on_branch="On branch ([^${IFS}]*)"
+#  local on_commit="HEAD detached at ([^${IFS}]*)"
+#
+#  if [[ $git_status =~ $on_branch ]]; then
+#    local branch=${BASH_REMATCH[1]}
+#    echo "($branch)"
+#  elif [[ $git_status =~ $on_commit ]]; then
+#    local commit=${BASH_REMATCH[1]}
+#    echo "($commit)"
+#  fi
+#}
 
 # I like putting angle-brackets around the branch name.  Customize to taste by
 # changing the punctuation around the \1 at the end of the line.
