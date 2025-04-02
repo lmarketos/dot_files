94d2a746ec68271d85383393911d30ecdc076b7a
LM <git@marketos.com>
2025-04-01 19:33:49 -0600

    update tmux.conf with plugins

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ mkdir -p ${XDG_CONFIG_HOME}
 mkdir -p ${XDG_BIN_HOME}
 stow  -v -t ${XDG_CONFIG_HOME} config
 stow  -v -t ${XDG_BIN_HOME} bin
+mkdir -p ${XDG_CONFIG_HOME}/tmux/plugins
+stow -v -t ${XDG_CONFIG_HOME}/tmux/plugins deps
